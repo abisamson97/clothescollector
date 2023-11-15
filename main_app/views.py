aa400f84784196ee98b467bfc905325c71db5a91
Abi Samson <abi.samson97@yahoo.com>
2023-11-15 17:01:42 -0500

    lab pt. 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+clothes = [
+  {'type': 'Pants', 'material': 'denim', 'description': 'black denim, wide-legged, baggy, no rips', 'size': 27},
+  {'type': 'T-shirt', 'material': 'cotton', 'description': 'cream graphic tee', 'size': 'Large'},
+]
 # Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def clothes_index(request):
+  return render(request, 'clothes/index.html', {
+    'clothes': clothes
+  })
